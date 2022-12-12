563b7100f3f7aa91fa2739643182bbef56547cc8
Alparslan <alparslanaydgn02@gmail.com>
2022-12-12 23:58:29 +0300

    Added "CarbonToKYC value updated"

--- src/app/component/carbon-to-kyc/carbon-to-kyc.component.ts
+++ src/app/component/carbon-to-kyc/carbon-to-kyc.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,FormControl,Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { CarbonToKyc } from 'src/app/models/carbonToKYC';
 import { CarbonToKycService } from 'src/app/services/carbon-to-kyc.service';
 
@@ -11,15 +13,45 @@ import { CarbonToKycService } from 'src/app/services/carbon-to-kyc.service';
 export class CarbonToKycComponent implements OnInit{
   carbonToKyc: CarbonToKyc[] = [];
   dataLoaded=false;
-  constructor(private carbontokycService: CarbonToKycService) {}
+  carbonToKycUpdateForm: FormGroup;
+  
+  constructor(private carbontokycService: CarbonToKycService,
+    private toastrService:ToastrService,
+    private formBuilder:FormBuilder
+    ) {}
+
 
   ngOnInit(): void {
     this.getCarbonToKycs();
+    this.createcarbonToKycUpdateForm();
   }
+  
+  createcarbonToKycUpdateForm(){
+    this.carbonToKycUpdateForm=this.formBuilder.group({
+      carbontoKYC:["",Validators.required],
+      id:[1]
+    });
+  }
+
   getCarbonToKycs() {
     this.carbontokycService.getCarbonToKycs().subscribe(response=>{
       this.carbonToKyc=response.data;
       this.dataLoaded=true;
     });
   }
+
+  update(){
+    if (this.carbonToKycUpdateForm.valid) {
+      let carbonToKycModel = Object.assign({},this.carbonToKycUpdateForm.value); 
+      this.carbontokycService.update(carbonToKycModel).subscribe(data=>{
+        this.toastrService.success(data.message,"Congratulations");
+      }); //observable
+      
+    } else {
+      this.toastrService.error("Enter the Information Completely","Be careful");
+    }
+  }
+
+
+
 }
--- src/app/services/carbon-to-kyc.service.ts
+++ src/app/services/carbon-to-kyc.service.ts
@@ -3,16 +3,23 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { CarbonToKyc } from '../models/carbonToKYC';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarbonToKycService {
-  apiUrl = 'https://localhost:44342/api/carbontokycs/getall';
+  apiUrl = 'https://localhost:44342/api/carbontokycs/';
   
+
   constructor(private httpClient: HttpClient) { }
 
   getCarbonToKycs():Observable<ListResponseModel<CarbonToKyc>> {
-    return this.httpClient.get<ListResponseModel<CarbonToKyc>>(this.apiUrl);
+    return this.httpClient.get<ListResponseModel<CarbonToKyc>>(this.apiUrl+"getall");
+  }
+
+
+  update(carbonToKYC:CarbonToKyc):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"update",carbonToKYC)
   }
 }
